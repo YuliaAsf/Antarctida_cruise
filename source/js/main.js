c4afd46ec05cbf48dc06e9feda665c2b2aacd170
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-07-28 05:46:38 +0300

    Correction

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 const button = document.querySelector('.page-header button');
 const container = document.querySelector('.page-header__wrapper');
+const link = document.querySelectorAll('.page-header__navigation a');
+const body = document.querySelector('body');
 
 container.classList.remove('page-header__menu-nojs');
 
@@ -7,8 +9,21 @@ button.addEventListener('click', ()=> {
   if (container.classList.contains('page-header__menu-closed')) {
     container.classList.remove('page-header__menu-closed');
     container.classList.add('page-header__menu-opened');
+    body.classList.add('no-scrolling');
   } else {
     container.classList.remove('page-header__menu-opened');
     container.classList.add('page-header__menu-closed');
+    body.classList.remove('no-scrolling');
   }
 });
+
+let i = 0;
+for (i = 0; i <= link.length; i++) {
+  link[i].addEventListener('click', ()=> {
+    if (container.classList.contains('page-header__menu-opened')) {
+      container.classList.remove('page-header__menu-opened');
+      container.classList.add('page-header__menu-closed');
+      body.classList.remove('no-scrolling');
+    }
+  });
+}
